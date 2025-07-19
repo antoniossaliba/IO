a4181005c44ce6ce6e6c9714b35415704988136b
Antonios Saliba <168730127+antoniossaliba@users.noreply.github.com>
2025-07-19 23:12:45 +0300

    IO read using Scanner | IO write using FileWriter, BufferedWriter, and PrintWriter

---
+++ FileReadFileWrite.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FileReadFileWrite {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("file.txt");
+
+        FileWriter fw = new FileWriter("newFile.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
+        PrintWriter pw = new PrintWriter(bw);
+
+        Scanner scanner = new Scanner(file);
+
+        while(scanner.hasNext()) {
+
+            fw.write(scanner.next());
+
+        }
+
+        fw.close();
+        bw.close();
+        pw.close();
+        scanner.close();
+
+    }
+}
